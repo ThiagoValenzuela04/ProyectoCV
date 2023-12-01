0cc9069af4aa1126d3456465ab8ba678b41081bb
ThiagoValenzuela04 <143789885+ThiagoValenzuela04@users.noreply.github.com>
2023-12-01 09:37:42 -0300

    VERSION FINAL

---
+++ datos_personales.js
@@ -0,0 +1,28 @@
+// Obtén el botón y el texto desplegable utilizando su ID
+const button = document.getElementById('myButton');
+const text = document.getElementById('text');
+
+// Agrega un evento de clic al botón
+button.addEventListener('click', function() {
+  // Verifica si el texto está oculto o visible
+  if (text.style.display === 'none') {
+    // Muestra el texto desplegable
+    text.style.display = 'block';
+  } else {
+    // Oculta el texto desplegable
+    text.style.display = 'none';
+
+  }
+});
+
+const colorButton = document.getElementById('colorButton');
+const body = document.body;
+
+colorButton.addEventListener('click', function() {
+  body.classList.toggle('white-background');
+});
+
+
+
+
+
